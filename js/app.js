98270b92cce8159acca54292f16d782e05d1eea8
DenizRuezgar <83456482+DenizRuezgar@users.noreply.github.com>
2021-12-10 09:57:46 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,91 @@
+const container = document.querySelector(".container");
+
+var Daten = new Array();
+if (localStorage.getItem("Daten") == null) {
+  var Daten = new Array();
+} else {
+  Daten = JSON.parse(localStorage.getItem("Daten"));
+}
+console.log(localStorage.getItem("Daten"));
+
+
+
+if (localStorage.getItem("count") == null) {
+  var count = 0;
+} else {
+  count = localStorage.getItem("count");
+}
+console.log(count);
+
+if (localStorage.getItem("wert") == null) {
+  var balance = 100;
+} else {
+  balance = localStorage.getItem("balance");
+}
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+};
+
+function safe() {
+  beschreibung = document.getElementById('Beschreibung').value;
+  datum = document.getElementById('Datum').value;
+  kategorie = document.getElementById('Kategorie').value;
+  wert = document.getElementById('Wert').value;
+
+  localStorage.setItem("beschreibung", beschreibung);
+  localStorage.setItem("datum", datum);
+  localStorage.setItem("kategorie", kategorie);
+  localStorage.setItem("wert", wert);
+
+  console.log("Gespeichert!");
+
+  console.log(localStorage.getItem("beschreibung"));
+  console.log(localStorage.getItem("datum"));
+  console.log(localStorage.getItem("kategorie"));
+  console.log(localStorage.getItem("wert"));
+  Daten[count] = new Object();
+  Daten[count]["Beschreibung"] = beschreibung;
+  Daten[count]["Datum"] = datum;
+  Daten[count]["Kategorie"] = kategorie;
+  Daten[count]["Wert"] = wert;
+  balance = parseInt(balance) - parseInt(wert);
+  Daten[count]["Balance"] = balance;
+  localStorage.setItem("balance", balance);
+  localStorage.setItem("Daten", JSON.stringify(Daten));
+  count++;
+  localStorage.setItem("count", count);
+  console.log(balance);
+}
+
+function ausgeben() {
+  var container = document.getElementById("ausgabe"),
+    dl;
+  if (container) {
+    dl = container.appendChild(document.createElement("dl"));
+    Daten.forEach(function (m, i) {
+      var dd, dt, eigenschaft;
+      dt = document.createElement("dt");
+      dt.innerHTML = "Daten: ";
+      dl.appendChild(dt);
+      for (eigenschaft in m) {
+        dd = document.createElement("dd");
+        dd.innerHTML = eigenschaft + ": " + m[eigenschaft];
+        dl.appendChild(dd);
+      }
+    });
+  }
+}
+
+function reload() {
+  location.reload();
+}
+
+
+
---
+++ serviceWorker.js
@@ -0,0 +1,41 @@
+var cacheName = 'deniz-pwab';
+var filesToCache = [
+  '/index.html',
+  'js/app.js'
+];
+
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    } )
+  );
+});
+
+self.addEventListener('activate', function(e)
+{
+console.log('[ServiceWorker] Activate');
+e.waitUntil(
+  caches.keys().then(function(keyList){
+    return Promise.all(keyList.map(function(key){
+      if(key !== cacheName){
+        console.log('[ServiceWorker] Removing old cache', key);
+        return caches.delete(key);
+      }
+    }));
+  })
+)
+return self.clients.claim();
+});
+
+/*
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
+*/
